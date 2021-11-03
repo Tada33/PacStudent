db5c29d482b1a2a5de8efca9f644ed8bbd046c53
Tada33 <tadanari33@gmail.com>
2021-11-04 01:10:51 +1100

    Updating Start Scene

---
+++ PacStudent/Assets/Scripts/startscenedragonmove.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startscenedragonmove : MonoBehaviour
+{
+    [SerializeField] Transform[] Positions;
+    [SerializeField] float speed;
+    Transform NextPos;
+
+    int NextPosIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NextPos = Positions[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveDragon();
+    }
+    void MoveDragon()
+    {
+        if (transform.position == NextPos.position)
+        {
+            NextPosIndex++;
+            if(NextPosIndex >= Positions.Length)
+            {
+                NextPosIndex = 0;
+            }
+            NextPos = Positions[NextPosIndex];
+        }
+        
+
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, NextPos.position, speed * Time.deltaTime);
+        }
+    }
+}
